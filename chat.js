42b340d139f973646ea046e989695785239b8429
John Hagstrand <john@hagstrand.com>
2018-10-14 04:37:48 +0000

    add chat

---
+++ chat.js
@@ -0,0 +1,111 @@
+/**
+	class BrowserHistory
+	@constructor
+**/
+voyc.Chat = function() {
+	this.eChat = {};
+	this.eEntry = {};
+	this.users = [];
+}
+
+voyc.Chat.posttemplate = "<div class='chatline clearfix'><div class='chatuser f%side%'>%user%:</div><div class='chatmsg f%side%'>%message%</div><div class='chattime f%side%'>%time%</div></div>";
+voyc.Chat.entrytemplate = "";
+
+voyc.Chat.prototype.setup = function(container) {
+	this.eChat = document.createElement('div');
+	this.eChat.classList.add('chat');
+	container.appendChild(this.eChat);
+
+	this.eEntry = document.createElement('div');
+	this.eEntry.classList.add('chatentry');
+	container.appendChild(this.eEntry);
+	
+	document.getElementById('guestpostbtn').addEventListener('click', function(e) {
+		var s = document.getElementById('guestpost').value;
+		voyc.chat.post(2, s); 
+		voyc.chat.post(1, 'ok'); 
+		document.getElementById('guestpost').value = '';
+	}, false);
+}
+
+voyc.Chat.prototype.addUser = function(name, host, me) {
+	var ndx = this.users.length;
+	var id = ndx + 1;
+	var host = host || false;
+	var me = me || false;
+	this.users.push({id:id, name:name, host:host, me:me});
+	return id;
+}
+
+/**
+	method post()
+	If multiple humans are using this,
+	each human will have his own instance of the screen,
+	with himself on the right, and the others on the left.
+	
+	In normal usage, Samantha is the host and I am the guest.
+	Sam is on the left, I am on the right.
+**/
+voyc.Chat.prototype.post = function(id, message, mchoice) {
+	var user = this.users[id - 1];
+	var side = (user.me == true) ? 'right' : 'left';
+	var name = (user.me == true) ? '' : user.name;
+
+	var dtime = new Date();
+	var hh = dtime.getHours();
+	var mm = dtime.getMinutes();
+	var smm = mm.toString();
+	smm = (smm.length > 1) ? smm : '0' + smm;
+	var stime = hh + ':' + smm;
+
+	// add message box to chat box
+	var s = voyc.Chat.posttemplate;
+	s = s.replace(/%side%/g, side);
+	s = s.replace(/%user%/g, name);
+	s = s.replace(/%message%/g, message);
+	s = s.replace(/%time%/g, stime);
+
+	var m = document.createElement('div');
+	m.innerHTML = s;
+	this.eChat.appendChild(m);
+
+	// add multiple choice options
+	var soptions = '';
+	document.getElementById('mchoices').innerHTML = '';
+	if (mchoice) {
+		for (var i=0; i<mchoice.length; i++) {
+			var opt = document.createElement('button');
+			opt.id = 'opt_' + i;
+			opt.innerHTML = mchoice[i];
+			document.getElementById('mchoices').appendChild(opt);
+			opt.addEventListener('click', function(e) {
+				var s = e.target.innerHTML;
+				voyc.chat.post(2,s);
+			}, false);
+		}
+	}
+}
+
+voyc.Chat.prototype.init = function(conversation) {
+	// draw chat window with all posts
+}
+
+voyc.Chat.prototype.save = function(name, conversation) {
+	// save conversation to local storage
+}
+
+voyc.Chat.prototype.restore = function(name) {
+	// restore conversation from local storage
+}
+
+/** 
+	save conversation on every post? 
+*/
+voyc.conversation = {
+	users: [
+		/* {name:'Samantha', host:true, me:false}, */
+	],
+	posts: [
+		/* {user:1, date:'24Jun2018', message:'hello' }, */
+	]
+};
